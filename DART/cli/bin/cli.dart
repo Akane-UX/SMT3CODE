25cbcf71992f12b8c5b3021634f536824cc629a0
Akane-UX <rtempest922@gmail.com>
2026-09-15 01:31:24 +0700

    cli

--- DART/cli/bin/cli.dart
+++ DART/cli/bin/cli.dart
@@ -1,12 +1,24 @@
+import 'dart:io';
+
 void main() {
-  int n = 10;
-  int a = 0;
-  int b = 1;
-  print("Fibonacci up to $n");
-  for (int i = 0; i < n; i++) {
-    print(a);
-    int next = a + b;
-    a = b;
-    b = next;
+  print('=== Program Cek Ganjil / Genap ===');
+  stdout.write('Masukkan sebuah angka: ');
+  
+  // Membaca input dari user
+  String? input = stdin.readLineSync();
+  
+  if (input != null && input.isNotEmpty) {
+    int? number = int.tryParse(input);
+    
+    if (number != null) {
+      // Kondisi untuk menentukan genap atau ganjil
+      if (number % 2 == 0) {
+        print('$number adalah bilangan GENAP.');
+      } else {
+        print('$number adalah bilangan GANJIL.');
+      }
+    } else {
+      print('Error: Input yang dimasukkan bukan angka!');
+    }
   }
 }
